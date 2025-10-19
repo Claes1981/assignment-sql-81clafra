c82a500930382ea8f32badc9d3d7926182834eb9
Claes Fransson <81clafra@gafe.molndal.se>
2025-10-19 20:34:53 +0200

    Partial implementation of the CRUD method to insert new monsters into the database.

--- Data/MonsterRepository.cs
+++ Data/MonsterRepository.cs
@@ -11,18 +11,21 @@ namespace assignment_sql_81clafra.Data
         // Implementera CRUD-metoder här
         // Create, GetAll, GetById, Update, Delete
 
-        static void CreatePerson()
+        static void CreateMonster()
         {
-            Console.WriteLine("\n--- Lägg till ny person ---");
+            Console.WriteLine("\n--- Lägg till nytt monster ---");
 
             // 1. Fråga användaren efter namn
             Console.Write("Namn: ");
             string? name = Console.ReadLine();
 
-            // 2. Fråga användaren efter ålder
-            Console.Write("Ålder: ");
-            if (!int.TryParse(Console.ReadLine(), out int age))
-                Console.WriteLine("Felaktig ålder!");
+            // 2. Fråga användaren efter typ av monster
+            Console.Write("Typ: ");
+            string? type = Console.ReadLine();
+
+            // 3. Fråga användaren efter riskgrad
+            Console.Write("Typ: ");
+            string? dangerLevel = Console.ReadLine();
 
             // 3. Skapa SQL-frågan med PARAMETRAR (inte string concatenation!)
             // Tipset: INSERT INTO Personer (Namn, Ålder) VALUES (@namn, @ålder)
@@ -31,17 +34,19 @@ namespace assignment_sql_81clafra.Data
             // 4. Öppna connection, skapa command, lägg till parametrar, kör ExecuteNonQuery()
             connection.Open();
 
-            string sqlCommandText = "INSERT INTO Personer (Namn, Ålder) VALUES (@name, @age)";
+            // TODO: Does Id need to be inserted?
+            string sqlCommandText = "INSERT INTO Monster (Name, Type, DangerLevel) VALUES (@name, @type, @dangerlevel)";
 
             using var sqlCommand = new SQLiteCommand(sqlCommandText, connection);
             sqlCommand.Parameters.AddWithValue("@name", name);
-            sqlCommand.Parameters.AddWithValue("@age", age);
+            sqlCommand.Parameters.AddWithValue("@type", type);
+            sqlCommand.Parameters.AddWithValue("@dangerlevel", dangerLevel);
 
             sqlCommand.ExecuteNonQuery();
 
             // 5. Skriv ut ett be1kräftelsemeddelande
 
-            Console.WriteLine("✅ Person tillagd!");
+            Console.WriteLine("✅ Monster tillagt!");
         }
     }
 }
