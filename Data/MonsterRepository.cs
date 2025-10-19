80cbabf1437f7d9ee0bb3e9a009fec4274bb4848
Claes Fransson <81clafra@gafe.molndal.se>
2025-10-19 20:18:14 +0200

    Start with code from previous excersise, to be modified for current application.

--- Data/MonsterRepository.cs
+++ Data/MonsterRepository.cs
@@ -10,5 +10,38 @@ namespace assignment_sql_81clafra.Data
     {
         // Implementera CRUD-metoder här
         // Create, GetAll, GetById, Update, Delete
+
+        static void CreatePerson()
+        {
+            Console.WriteLine("\n--- Lägg till ny person ---");
+
+            // 1. Fråga användaren efter namn
+            Console.Write("Namn: ");
+            string? name = Console.ReadLine();
+
+            // 2. Fråga användaren efter ålder
+            Console.Write("Ålder: ");
+            if (!int.TryParse(Console.ReadLine(), out int age))
+                Console.WriteLine("Felaktig ålder!");
+
+            // 3. Skapa SQL-frågan med PARAMETRAR (inte string concatenation!)
+            // Tipset: INSERT INTO Personer (Namn, Ålder) VALUES (@namn, @ålder)
+            using SQLiteConnection connection = new SQLiteConnection(sqlConnectionString);
+
+            // 4. Öppna connection, skapa command, lägg till parametrar, kör ExecuteNonQuery()
+            connection.Open();
+
+            string sqlCommandText = "INSERT INTO Personer (Namn, Ålder) VALUES (@name, @age)";
+
+            using var sqlCommand = new SQLiteCommand(sqlCommandText, connection);
+            sqlCommand.Parameters.AddWithValue("@name", name);
+            sqlCommand.Parameters.AddWithValue("@age", age);
+
+            sqlCommand.ExecuteNonQuery();
+
+            // 5. Skriv ut ett be1kräftelsemeddelande
+
+            Console.WriteLine("✅ Person tillagd!");
+        }
     }
 }
