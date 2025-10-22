5d5e2d6fafe3e5980d2fcc35688ca6f8fd422553
Claes Fransson <81clafra@gafe.molndal.se>
2025-10-22 19:22:11 +0200

    Preparation of data model classes.

---
+++ Models/Hunter.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment_sql_81clafra.Models
+{
+    public class Hunter
+    {
+        
+    }
+}
