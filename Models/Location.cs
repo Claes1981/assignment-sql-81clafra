9b06649a580a5699583548cfb82295586ff156a5
Claes Fransson <81clafra@gafe.molndal.se>
2025-10-27 20:11:34 +0100

    Implement model class for locations.

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,12 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace assignment_sql_81clafra.Models
 {
     public class Location
     {
-        
+        // Parameterless constructor for 'new' locations before DB insert
+        // Perplexity: https://www.perplexity.ai/search/can-you-help-me-with-this-erro-1Vj.QVQzSbOfN8bbixFCdg#2
+        public Location() { }
+
+        // Generated with help from Visual Studio Code
+        public Location(int id, string name, string region)
+        {
+            Id = id;
+            Name = name;
+            Region = region;
+        }
+
+        // Generated with help from TabbyML/Qwen2.5-Coder-7B-Instruct
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
     }
 }
